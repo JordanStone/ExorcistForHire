5b244cd63ac2c58c503bdb0e8990036db237d253
Chenghao Wang <turnalan@qq.com>
2016-02-27 21:27:25 -0500

    Add opening animation (The prefeb openingscenetriger is the one which create the animation, if need, just drag the prefeb to the scene)

---
+++ Assets/_Scripts/StoryScripts/OpeningScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class OpeningScript : MonoBehaviour {
+
+	private GameObject animtraiger;
+	private bool zoomin;
+
+	// Use this for initialization
+	void Start () {
+	
+		zoomin=false;
+		animtraiger = GameObject.Find("OpeningSceneTriger");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if(GameObject.Find("OpeningSceneTriger")) {
+
+
+
+			transform.LookAt(animtraiger.transform);
+		
+		
+			if(animtraiger.GetComponent<OpenningScript2>().timetozoomin){
+				this.gameObject.GetComponent<Camera>().fieldOfView = Mathf.Lerp(this.gameObject.GetComponent<Camera>().fieldOfView,10,Time.deltaTime);
+			}
+			
+			if(animtraiger.GetComponent<OpenningScript2>().timetozoomout){
+				this.gameObject.GetComponent<Camera>().fieldOfView = Mathf.Lerp(this.gameObject.GetComponent<Camera>().fieldOfView,60,Time.deltaTime);
+			}
+
+			if(animtraiger.GetComponent<OpenningScript2>().timetoguyplay){
+				//add gun animation here
+				//add conversation here
+			}
+		
+		
+		
+		
+		}
+
+
+	
+	}
+}
---
+++ Assets/_Scripts/StoryScripts/OpenningScript2.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class OpenningScript2 : MonoBehaviour {
+
+    public bool timetozoomin;
+    public bool timetozoomout;
+	public bool timetoguyplay;
+
+
+	// Use this for initialization
+	void Start () {
+		timetozoomin=false;
+		timetozoomout=false;
+		timetoguyplay=false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void timeto(){
+		timetozoomin = true;
+	}
+
+	public void gunplay(){
+		timetoguyplay = true;
+	}
+	
+	public void timeout(){
+		timetozoomout=true;
+	}
+
+
+	public void destoryOpen(){
+		Destroy(this.gameObject);
+	}
+}
