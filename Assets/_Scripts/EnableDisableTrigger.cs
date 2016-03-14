baa8e60085c1df1887f02c9dbc1e496f88c48f92
John Gall <Tigerquan@aol.com>
2016-03-14 00:44:03 -0400

    Populate the town with cultists.
    There may not be enough cultists, but things get really crazy really
    fast any way. Additionally, the church is gated until the ambush at City
    hall.

---
+++ Assets/_Scripts/EnableDisableTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableDisableTrigger : MonoBehaviour {
+	public GameObject[] Enable;
+	public GameObject[] Disable;
+	void OnTriggerEnter(Collider col) {
+		if(col.gameObject.name == "Player") {
+			foreach(GameObject g in Enable) {
+				g.SetActive(true);
+			}
+			foreach(GameObject g in Disable) {
+				g.SetActive(false);
+			}
+		}
+	}
+}
